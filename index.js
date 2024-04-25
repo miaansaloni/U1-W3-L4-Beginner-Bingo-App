64d5f27a8c34ff3d064824e9f2b01cda21a30f64
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-04-25 17:57:35 +0200

    adds js code improvements

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const tableEl = document.getElementById("innerTable");
 const randNumBTN = document.getElementById("randNum");
+const numDisplay = document.getElementById("numDisplay");
 
-for (let i = 1; i <= 76; i++) {
+let extractedNumbers = [];
+
+for (let i = 1; i <= 90; i++) {
   const cell = document.createElement("div");
   cell.className = "cell";
   cell.innerText = i;
@@ -9,11 +12,17 @@ for (let i = 1; i <= 76; i++) {
 }
 
 function randNumExtractor() {
-  return Math.floor(Math.random() * 76) + 1;
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * 90) + 1;
+  } while (extractedNumbers.includes(randomNumber));
+  extractedNumbers.push(randomNumber);
+  return randomNumber;
 }
 
 randNumBTN.addEventListener("click", function () {
   const randomNumber = randNumExtractor();
+  numDisplay.innerText = randomNumber;
   const selectedCell = document.querySelector(".cell:nth-child(" + randomNumber + ")");
   selectedCell.style.backgroundColor = "#ffb703";
   selectedCell.style.color = "white";
